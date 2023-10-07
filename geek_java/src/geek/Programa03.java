233f90ebe2670108be3d93ea3b07781f75afd94f
gabrielestefono <gabrielestefono@hotmail.com>
2023-10-07 18:04:36 -0300

    A função main

---
+++ geek_java/src/geek/Programa03.java
@@ -0,0 +1,10 @@
+package geek;
+
+// Linguagem orientada a objeto
+
+public class Programa03 {
+	// Essa função que torna a classe java um programa java
+	public static void main(String[] args){
+		System.out.println("Qualquer texto");
+	}
+}
