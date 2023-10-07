37df5ba43c9819c5d05af2a2b6dcb28f59291280
gabrielestefono <gabrielestefono@hotmail.com>
2023-10-07 12:38:34 -0300

    Compilador Bytecode

---
+++ geek_java/src/geek/Programa02.java
@@ -0,0 +1,10 @@
+// O compilador e o bytecode
+
+package geek;
+
+public class Programa02 {
+	public static void main(String[] args){
+		int idade = 26;
+		System.out.println("Eu tenho " + idade + " anos.");
+	}
+}
