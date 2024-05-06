75ac9d87704a87755f67e5eb59b37a34c13f48bc
Gabriel Estéfono <gabrielestefono@hotmail.com>
2024-05-06 09:26:27 -0300

    Ternários

---
+++ Secao_05/06_expressao_ternaria/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+	public static void main(String[] args) {
+		System.out.println(2 > 4 ? 50 : 80);
+		double preco = 34.5;
+		System.out.println((preco < 20.0) ? preco * 0.1 : preco * 0.05);
+	}
+}
