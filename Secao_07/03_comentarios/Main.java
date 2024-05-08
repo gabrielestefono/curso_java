66978c00abaa7dc41c359ef65cee4583845ec790
Gabriel Estéfono <gabrielestefono@hotmail.com>
2024-05-08 10:32:24 -0300

    Comentários

---
+++ Secao_07/03_comentarios/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+	public static void main(String[] args) {
+		// Comentário de linha
+
+		/*
+		 * Comentário de
+		 * Bloco
+		 */
+
+		/**
+		 * Comentários de documentação talvez? No PHP é
+		 */
+	}
+}
