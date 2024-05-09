95ad92d6574565dda30f8b7c8b778036b4a76486
Gabriel Estéfono <gabrielestefono@hotmail.com>
2024-05-09 10:57:12 -0300

    Começando Segundo Exemplo

---
+++ Secao_08/04_estoque/src/main/java/com/Program.java
@@ -0,0 +1,13 @@
+package com;
+
+import entities.Product;
+
+public class Program {
+
+    public static void main(String[] args) {
+        Product product = new Product();
+        product.name = "TV";
+        product.price = 700;
+        product.quantity = 50;
+    }
+}
