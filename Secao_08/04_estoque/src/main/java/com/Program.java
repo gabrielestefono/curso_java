1b71ec8090faec773226b4715a4845e6a4217c99
Gabriel Estéfono <gabrielestefono@hotmail.com>
2024-05-09 11:18:17 -0300

    Finalizando Exemplo

--- Secao_08/04_estoque/src/main/java/com/Program.java
+++ Secao_08/04_estoque/src/main/java/com/Program.java
@@ -7,8 +7,14 @@ public class Program {
     public static void main(String[] args) {
         Product product = new Product();
         product.name = "TV";
-        product.price = 700;
-        product.quantity = 50;
-        System.out.println(product.toString());
+        product.price = 900;
+        product.quantity = 10;
+        System.out.println(product);
+        System.out.println("Adicionando 5 itens ao estoque");
+        product.addProducts(5);
+        System.out.println(product);
+        System.out.println("Removendo 3 itens do estoque");
+        product.removeProducts(3);
+        System.out.println(product);
     }
 }
