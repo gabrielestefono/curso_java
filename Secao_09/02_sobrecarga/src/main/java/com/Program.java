ba2761d8b1f58f976c7260229acc3f318acf35e8
Gabriel Estéfono <gabrielestefono@hotmail.com>
2024-05-10 07:49:17 -0300

    Seção 09 - Aula 02

---
+++ Secao_09/02_sobrecarga/src/main/java/com/Program.java
@@ -0,0 +1,19 @@
+package com;
+
+import entities.Product;
+
+public class Program {
+
+    public static void main(String[] args) {
+        String name = "TV";
+        double price = 900.00;
+        Product product = new Product(name, price);
+        System.out.println(product);
+        System.out.println("Adicionando 5 itens ao estoque");
+        product.addProducts(5);
+        System.out.println(product);
+        System.out.println("Removendo 3 itens do estoque");
+        product.removeProducts(3);
+        System.out.println(product);
+    }
+}
