0cce0c0aa1311f1a7b0dd261619075ad94de72b2
Gabriel Estéfono <gabrielestefono@hotmail.com>
2024-05-10 07:29:22 -0300

    Finalizando Seção

--- Secao_08/06_radius_estatico/src/main/java/com/Program.java
+++ Secao_08/06_radius_estatico/src/main/java/com/Program.java
@@ -4,9 +4,8 @@ import utils.Calculator;
 
 public class Program {
     public static void main(String[] args) {
-        Calculator calc = new Calculator();
-        double circunference = calc.circunference(3.0);
-        double volume = calc.circunference(3.0);
+        double circunference = Calculator.circunference(3.0);
+        double volume = Calculator.circunference(3.0);
         System.out.println(circunference);
         System.out.println(volume);
     }
