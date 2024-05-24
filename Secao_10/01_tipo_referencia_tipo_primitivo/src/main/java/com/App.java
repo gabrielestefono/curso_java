cbccf06e68b5f850fb4dea6d5afcef5979f37751
Gabriel Estéfono <gabrielestefono@hotmail.com>
2024-05-24 17:02:40 -0300

    Reading Documentation

---
+++ Secao_10/01_tipo_referencia_tipo_primitivo/src/main/java/com/App.java
@@ -0,0 +1,15 @@
+package com;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+public static void main( String[] args )
+  {
+    int p;
+    p = 10;
+    System.out.println(p);
+  }
+}
---
+++ Secao_10/02_vetores_parte_1/src/main/java/com/App.java
@@ -0,0 +1,23 @@
+package com;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+public static void main( String[] args )
+  {
+    int n = 3;
+    double[] vect = new double[n];
+    vect[0] = 1.72;
+    vect[1] = 1.56;
+    vect[2] = 1.80;
+    double sum = 0.0;
+    for(int i = 0; i < n; i++){
+      sum += vect[i];
+    }
+    double avg = sum / n;
+    System.out.println(avg);
+  }
+}
