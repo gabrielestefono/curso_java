8f654baa288fba053fd8b8e3ecc0e6c90d7e9f89
Gabriel Estéfono <gabrielestefono@hotmail.com>
2024-05-30 16:08:09 -0300

    Listas Aula 01

---
+++ Secao_10/06_lists_1/src/main/java/com/example/App.java
@@ -0,0 +1,11 @@
+package com.example;
+
+/**
+ * Hello world!
+ *
+ */
+public class App{
+  public static void main( String[] args ){
+    // List x = new List(); // Interface e não pode ser instanciada
+  }
+}
