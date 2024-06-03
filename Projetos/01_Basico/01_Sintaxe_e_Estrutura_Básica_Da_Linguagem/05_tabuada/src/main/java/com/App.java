efeb9beb32ad36bd376513099c1bc39be56fb603
Gabriel Estéfono <gabrielestefono@hotmail.com>
2024-06-03 06:57:26 -0300

    Projeto Tabuada

--- Projetos/01_Basico/01_Sintaxe_e_Estrutura_Básica_Da_Linguagem/05_tabuada/src/main/java/com/App.java
+++ Projetos/01_Basico/01_Sintaxe_e_Estrutura_Básica_Da_Linguagem/05_tabuada/src/main/java/com/App.java
@@ -1,13 +1,32 @@
 package com;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import java.util.Scanner;
+
+public class App{
+  public static void main( String[] args ){
+    System.out.println( "Digite um numero de 0 a 10");
+    try (Scanner sc = new Scanner(System.in)) {
+      Integer valor = sc.nextInt();
+      for(int i = 0; i < 4; i++){
+        for(int j = 0; j < 11; j++){
+          switch (i) {
+            case 0:
+              System.out.println(valor + " * " + j + " = " + (valor * j));
+              break;
+            case 1:
+              if(j != 0) System.out.println(valor + " / " + j + " = " + (valor / j));
+              break;
+            case 2:
+              System.out.println(valor + " + " + j + " = " + (valor + j));
+              break;
+            case 3:
+              System.out.println(valor + " - " + j + " = " + (valor - j));
+              break;
+            default:
+              break;
+          }
+        }
+      }
     }
+  }
 }
